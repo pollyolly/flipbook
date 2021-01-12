8e519bed09413d7f463d428f398b9801eaf08487
John Mark <johnmarkroco05@gmail.com>
2021-01-13 02:06:15 +0800

    Change schema 01/13/2021

--- schema.sql
+++ schema.sql
@@ -1,38 +1,33 @@
-DROP TABLE IF EXISTS tbl_logs;
-create table tbl_logs (
-id int AUTO_INCREMENT PRIMARY KEY,
-ip_address varchar(50),
-username varchar(50),
-log_date date,
-activity varchar(200)
+create table tbl_log(
+id int primary key auto_increment,
+    ip_address varchar(15),
+    log_date varchar(50),
+    activity varchar(100),
+    user_id int
 );
-DROP TABLE IF EXISTS tbl_ldap;
-create table tbl_ldap (
-id int AUTO_INCREMENT PRIMARY KEY,
-host varchar(50),
-port char(3),
-filter_by date,
-country char(2)
+
+create table tbl_ldap(
+id int primary key auto_increment,
+    host varchar(50),
+    port int,
+    filter_by varchar(50),
+    organization varchar(100),
+    country char(3)
 );
-DROP TABLE IF EXISTS tbl_flipbook;
-create table tbl_flipbook (
-id int AUTO_INCREMENT PRIMARY KEY,
-user_id int,
-filname varchar(500),
-public_link varchar(500),
-date_upload date,
-status varchar(20)
+
+create table tbl_user(
+id int primary key AUTO_INCREMENT,
+    username varchar(50),
+    department varchar(50),
+    type varchar(20),
+    user_status varchar(10)
 );
-DROP TABLE IF EXISTS tbl_template;
-create table tbl_template (
-id int AUTO_INCREMENT PRIMARY KEY,
-title varchar(500),
-abstract varchar(500),
-flipbooklink varchar(500),
-flipbook_title varchar(500),
-college_abbrevation varchar(500),
-department varchar(500),
-years varchar(500),
-subject_field varchar(1000),
-subject_sub_field varchar(1000)
+
+create table tbl_flipbook(
+id int PRIMARY key AUTO_INCREMENT,
+    filename varchar(100),
+    public_link varchar(100),
+    date_uploaded varchar(50),
+    flipbook_status varchar(20),
+    user_id int
 );
